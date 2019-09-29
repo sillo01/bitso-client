ee45bb81111bc62f10ec42cb17fefc227cb6a098
Alansillo <ramos.acrp@gmail.com>
2019-09-29 08:50:12 -0500

    added sort enum

---
+++ Models/Enums.cs
@@ -0,0 +1,8 @@
+namespace bitso_client.Models
+{
+    public enum Sort
+    {
+        Asc,
+        Desc
+    }
+}
--- Models/Trades/Payload.cs
+++
@@ -1,7 +0,0 @@
-namespace bitso_client.Models.Trades
-{
-    public class Payload
-    {
-        public int MyProperty { get; set; }
-    }
-}
