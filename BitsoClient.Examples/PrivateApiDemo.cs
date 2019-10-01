c71c77975b357f8b2ebb27842a7292dbed526780
Alansillo <ramos.acrp@gmail.com>
2019-10-01 12:00:36 -0500

    PublicApiDemo

---
+++ BitsoClient.Examples/PrivateApiDemo.cs
@@ -0,0 +1,15 @@
+using BitsoClient.RestApi;
+
+namespace BitsoClient.Examples
+{
+    public class PrivateApiDemo
+    {
+        public PrivateApiDemo()
+        {
+            ClientConfiguration config = new ClientConfiguration()
+            {
+                
+            };
+        }
+    }
+}
---
+++ BitsoClient.Examples/PublicApiDemo.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using BitsoClient.RestApi;
+
+namespace BitsoClient.Examples
+{
+    public class PublicApiDemo
+    {
+        private readonly PublicApiClient client;
+        private readonly string baseUrl = "https://api.bitso.com/v3/";
+
+        public PublicApiDemo(HttpClient client)
+        {
+            var requester = new HttpRequester(client);
+            this.client = new PublicApiClient(requester, baseUrl);
+        }
+
+        public async Task PrintOrderBook()
+        {
+            var payload = await client.GetOrderBook("btc_mxn");
+
+            Console.WriteLine(payload);
+        }
+    }
+}
