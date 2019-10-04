472e34f996d90429b6e7be406c98c7ae24dda4cd
Alan Perez <alan.perez@pressganey.com>
2019-10-03 22:28:12 -0500

    added api version to client configuration

--- BitsoClient/RestApi/ClientConfiguration.cs
+++ BitsoClient/RestApi/ClientConfiguration.cs
@@ -5,14 +5,17 @@ namespace BitsoClient.RestApi
         public string BaseUrl { get; }
         public string ApiKey { get; }
         public string ApiSecret { get; }
+        public string ApiVersion { get; set; }
 
         public ClientConfiguration(
             string url,
+            string version,
             string key,
             string secret
         )
         {
             BaseUrl = url;
+            ApiVersion = version;
             ApiKey = key;
             ApiSecret = secret;
         }
