dd3e4411a802af4a45b0d05deeb95eafef9b5aa7
Alan Perez <alan.perez@pressganey.com>
2019-10-03 22:17:28 -0500

    updated RequestOptions tests

--- BitsoClient.Tests/RestApi/RequestOptionsTests.cs
+++ BitsoClient.Tests/RestApi/RequestOptionsTests.cs
@@ -11,14 +11,14 @@ namespace BitsoClient.Tests.RestApi
         [Fact]
         public void GetsRequestSignature()
         {
-            long fakeNonce = 1234L;
+            long fakeNonce = 12341234;
             string fakeSecret = "secret-password";
             byte[] encodedSecret = Encoding.UTF8.GetBytes(fakeSecret);
 
             RequestOptions options = new RequestOptions("get", "/url/path");
             using (HMACSHA256 hmac = new HMACSHA256(encodedSecret))
             {
-                string expectedSignature = "283c1a46963f728ae9e3dd3beb1dfd3d0135fb6fda62492fd0005d9ac48bde3d";
+                string expectedSignature = "ecfb6723dc3223cd04e29833e11033cf556788719cf1d729a9cde8feb0b5b34d";
                 string hexSignature = options.GetSignature(fakeNonce, hmac);
                 Assert.Equal(expectedSignature, hexSignature);
             }
