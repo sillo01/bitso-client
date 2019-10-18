0521c4f9406866acb07617ee89e9ff8a5c4490e5
Alan Ramos <ramos.acrp@gmail.com>
2019-10-17 22:09:20 -0500

    new order model

--- BitsoClient/Models/Orders/NewOrder.cs
+++ BitsoClient/Models/Orders/NewOrder.cs
@@ -2,11 +2,20 @@ namespace BitsoClient.Models.Orders
 {
     public class NewOrder
     {
+        public NewOrder(string book, string side, string type)
+        {
+            this.book = book;
+            this.side = side;
+            this.type = type;
+        }
         public string book { get; set; }
         public string side { get; set; }
         public string type { get; set; }
         public string major { get; set; }
         public string minor { get; set; }
         public string price { get; set; }
+        public string stop { get; set; }
+        public string time_in_force { get; set; }
+        public string client_id { get; set; }
     }
 }
