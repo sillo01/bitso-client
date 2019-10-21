8171afff3d1e45d55e64adb27223f66b7fe37466
Alansillo <ramos.acrp@gmail.com>
2019-10-20 19:32:08 -0500

    OrderBook property names

--- BitsoClient/Models/OrderBook/Order.cs
+++ BitsoClient/Models/OrderBook/Order.cs
@@ -1,29 +1,20 @@
+using Newtonsoft.Json;
+
 namespace BitsoClient.Models.OrderBook
 {
     public class Order
     {
-        public string book { get; set; }
-        public string price { get; set; }
-        public string amount { get; set; }
+        [JsonPropertyAttribute("book")]
+        public string Book { get; set; }
+        [JsonPropertyAttribute("book")]
+        public string Price { get; set; }
+        [JsonPropertyAttribute("book")]
+        public string Amount { get; set; }
 
-        public double pPrice
-        {
-            get
-            {
-                return double.Parse(price);
-            }
-        }
-        public double pAmount
-        {
-            get
-            {
-                return double.Parse(amount);
-            }
-        }
 
         public override string ToString()
         {
-            return $"Price ${this.price}, Amount {this.amount}, Book {this.book}";
+            return $"Price ${this.Price}, Amount {this.Amount}, Book {this.Book}";
         }
     }
 }
