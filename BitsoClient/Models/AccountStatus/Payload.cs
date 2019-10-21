aef0fce84e9767cf9e57d872c3ec2f0b64f70e05
Alansillo <ramos.acrp@gmail.com>
2019-10-20 19:29:16 -0500

    Status payload property names

--- BitsoClient/Models/AccountStatus/Payload.cs
+++ BitsoClient/Models/AccountStatus/Payload.cs
@@ -1,21 +1,38 @@
+using Newtonsoft.Json;
+
 namespace BitsoClient.Models.AccountStatus
 {
     public class Payload
     {
-        public string client_id { get; set; }
-        public string first_name { get; set; }
-        public string last_name { get; set; }
-        public string status { get; set; }
-        public string daily_limit { get; set; }
-        public string monthly_limit { get; set; }
-        public string daily_remaining { get; set; }
-        public string monthly_remaining { get; set; }
-        public string cell_phone { get; set; }
-        public string cell_phone_stored { get; set; }
-        public string email_stored { get; set; }
-        public string official_id { get; set; }
-        public string proof_of_residency { get; set; }
-        public string signed_contract { get; set; }
-        public string origin_of_funds { get; set; }
+        [JsonPropertyAttribute("client_id")]
+        public string ClientId { get; set; }
+        [JsonPropertyAttribute("first_name")]
+        public string FirstName { get; set; }
+        [JsonPropertyAttribute("last_name")]
+        public string LastName { get; set; }
+        [JsonPropertyAttribute("status")]
+        public string Status { get; set; }
+        [JsonPropertyAttribute("daily_limit")]
+        public string DailyLimit { get; set; }
+        [JsonPropertyAttribute("monthly_limit")]
+        public string MonthlyLimit { get; set; }
+        [JsonPropertyAttribute("daily_remaining")]
+        public string DailyRemaining { get; set; }
+        [JsonPropertyAttribute("monthly_remaining")]
+        public string MonthlyRemaining { get; set; }
+        [JsonPropertyAttribute("cell_phone")]
+        public string CellPhone { get; set; }
+        [JsonPropertyAttribute("cell_phone_stored")]
+        public string CellPhoneStored { get; set; }
+        [JsonPropertyAttribute("email_stored")]
+        public string EmailStored { get; set; }
+        [JsonPropertyAttribute("official_id")]
+        public string OfficialId { get; set; }
+        [JsonPropertyAttribute("proof_of_residency")]
+        public string ProofOfResidency { get; set; }
+        [JsonPropertyAttribute("signed_contract")]
+        public string SignedContract { get; set; }
+        [JsonPropertyAttribute("origin_of_funds")]
+        public string OriginOfFunds { get; set; }
     }
 }
