aca6606eb6770454d999e7a067534fe4ae7162ce
Alansillo <ramos.acrp@gmail.com>
2019-10-20 19:35:35 -0500

    OrderBook payload propery names

--- BitsoClient/Models/OrderBook/Payload.cs
+++ BitsoClient/Models/OrderBook/Payload.cs
@@ -1,14 +1,19 @@
 using System;
+using Newtonsoft.Json;
 
 namespace BitsoClient.Models.OrderBook
 {
     public class Payload
     {
+        [JsonPropertyAttribute("asks")]
         public Order[] Asks { get; set; }
+        [JsonPropertyAttribute("bids")]
         public Order[] Bids { get; set; }
 
-        public DateTime updated_at { get; set; }
-        public string sequence { get; set; }
+        [JsonPropertyAttribute("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+        [JsonPropertyAttribute("sequence")]
+        public string Sequence { get; set; }
 
         public override string ToString()
         {
