96582d2e579cdace525a1f4bac09a4632b6eb770
Alansillo <ramos.acrp@gmail.com>
2019-10-20 19:20:43 -0500

    Balance propertiy names

--- BitsoClient/Models/AccountBalance/Balance.cs
+++ BitsoClient/Models/AccountBalance/Balance.cs
@@ -1,10 +1,16 @@
+using Newtonsoft.Json;
+
 namespace BitsoClient.Models.AccountBalance
 {
     public class Balance
     {
-        public string currency { get; set; }
-        public string total { get; set; }
-        public string locked { get; set; }
-        public string available { get; set; }
+        [JsonPropertyAttribute("currency")]
+        public string Currency { get; set; }
+        [JsonPropertyAttribute("total")]
+        public string Total { get; set; }
+        [JsonPropertyAttribute("locked")]
+        public string Locked { get; set; }
+        [JsonPropertyAttribute("available")]
+        public string Available { get; set; }
     }
 }
